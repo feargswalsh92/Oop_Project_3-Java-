f1df4139464045931bcdce05678b97f68e29bc69
feargswalsh92 <fwalsh1@luc.edu>
2016-06-24 17:03:00 -0500

    PushTest

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -17,8 +17,8 @@ public class Draw implements Visitor<Void> {
 	private final Paint paint;
 
 	public Draw(final Canvas canvas, final Paint paint) {
-		this.canvas = canvas; // FIXME
-		this.paint = paint; // FIXME
+		this.canvas = canvas; // FIXED
+		this.paint = paint; // FIXED
 		paint.setStyle(Style.STROKE);
 	}
 
