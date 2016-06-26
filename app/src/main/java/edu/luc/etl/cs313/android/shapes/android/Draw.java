3463434caaa4b49ca5a6a467daa20544cf9eeaac
Jessica Brennan <Jessica@Jessica-Brennans-MacBook-Pro.local>
2016-06-26 11:09:52 -0500

    I only did onRectangle, not sure if it's correct. I'll have more time tonight or during the day tomorrow to look more into it.

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -38,7 +38,7 @@ public class Draw implements Visitor<Void> {
 	@Override
 	public Void onFill(final Fill f) {
 
-		//return null;
+		return null;
 
 	}
 
@@ -56,7 +56,7 @@ public class Draw implements Visitor<Void> {
 
 	@Override
 	public Void onRectangle(final Rectangle r) {
-
+		canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
 		return null;
 	}
 
